445dc2fdfccd488ae5d5a1ba50d3a62049281e9c
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-08-09 19:16:48 +0530

    :lipstick: style(Our Work)
    - Styled the our work page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,11 +1,65 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+// Styles
+import styled from "styled-components";
+
+// Images
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+
 const OurWork = () => {
   return (
-    <div>
-      <h1>Our Work</h1>
-    </div>
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="An Athlete" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="A Racer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="A couple" />
+        </Link>
+      </Movie>
+    </Work>
   );
 };
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #ccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 
 export default OurWork;
